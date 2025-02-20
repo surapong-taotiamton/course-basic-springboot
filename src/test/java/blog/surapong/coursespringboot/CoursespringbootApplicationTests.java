340a382ec6592538b30a34e9694d04b96bf522aa
surapong <surapong@netbay.co.th>
2025-02-20 19:54:11 +0700

    init project

---
+++ src/test/java/blog/surapong/coursespringboot/CoursespringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package blog.surapong.coursespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursespringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
